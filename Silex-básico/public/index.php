9d59c185c28b1ac1c8499c5b8029bd759967e951
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-01-18 10:15:01 -0300

    Rotas GET e POST

--- Silex-básico/public/index.php
+++ Silex-básico/public/index.php
@@ -8,4 +8,15 @@ $app->get('\hello world', function(){
     return "Hello World!! Minha primeira aplicação com Silex!";
 });
 
+$app->get('/home', function(){
+    return "<form method='post' action='/home'>
+        <input type='text' name='name'/>
+        <button type='submit'>Enviar</button>    
+    </form>";
+});
+
+$app->post('/home', function(){
+    return "Post foi enviado!";
+});
+
 $app->run();
