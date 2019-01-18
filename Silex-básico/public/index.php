759bff39f8e55b92c7b9d37c425dc8f6069910f4
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-01-18 12:47:42 -0300

    Parâmetros de rotas

--- Silex-básico/public/index.php
+++ Silex-básico/public/index.php
@@ -9,17 +9,20 @@ $app->get('\hello world', function(){
 });
 
 $app->get('/home', function(){
-    return "<form method='post' action='/home'>
+    return "<form method='post' action='/get-name/son/valor2'>
         <input type='text' name='name'/>
         <button type='submit'>Enviar</button>    
     </form>";
 });
 
-$app->post('/home', function(){
+$app->post('/get-name/{param1}/{param2}', function(Request $request, $param2, $param1){
     $name = $request->get('name','sem nome');
-    $data = $request->request->all();
-    print_r($data);die();
-    return new Response("Post foi enviado! Name:$name", 404);
+    
+    return "Post foi enviado.
+        <br/>Name:$name.
+        <br/>Param1: $param1.
+        <br/>Param2: $param2.
+        ";
 });
 
 $app->run();
