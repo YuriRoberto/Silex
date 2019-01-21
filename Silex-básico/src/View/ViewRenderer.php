a0ecd377b958d2f1c5fcac4d26d5214c2911b863
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-01-21 14:27:22 -0300

    Renderizando layout

--- Silex-básico/src/View/ViewRenderer.php
+++ Silex-básico/src/View/ViewRenderer.php
@@ -10,12 +10,25 @@ class ViewRenderer {
         $this->pathTemplates = $pathTemplates;
     }
 
-    public function render($name, array $data){
+    public function render($name, array $data = []){
+        $content = $this->getOutput($name, $data);
+        $layout = __DIR__.'/../../templates/layouts/layout.phtml';
+        return $this->getOutput($layout, ['content' => $content]);
+        
+    }
+
+    protected function getOutput($name, array $data = []){
+        $this->templateName = $name;
         extract($data);
         ob_start();
-        include $this->pathTemplates . '/$name.phtml';
-        $saida = ob_get_clean();
-        return $saida;
+        if(!file_exists($template)){
+            include $this->pathTemplates . "/{$this->templateNmae}.phtml";
+        }else{
+            $this->templateName = $template;
+            include $this->templateName;
+        }
+        $output = ob_get_clean();
+        return $output;
     }
 
 }
