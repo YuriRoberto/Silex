2241023462e1ef834bb1da0cb925fbefeab17d8e
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-01-24 12:05:27 -0300

    Estilizando Bootstrap CSS com Less

--- Silex-básico/gulpfile.js
+++ Silex-básico/gulpfile.js
@@ -2,20 +2,34 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const less = require('gulp-less');
 
+let DIR = {
+    less: './assets/less',
+    build: './public/build'
+};
+
+
+
+DIR.build_css = `${DIR.build}/css`;
+
 gulp.task('browser-sync', function(){
     browserSync.init({
         proxy: "localhost:8000",
         files: [
             "src/**/**.php",
             "public/index.php",
-            "templates/**/*.phtml"
+            "templates/**/*.phtml",
+            `${DIR.build_css}/**/*.css`
         ]
     })
 });
 
 gulp.task('less', function(){
-    gulp.src('./assets/less/style.less').pipe(less()).pipe(gulp.dest('./public/build/css'))
+    gulp.src(`${DIR.less}/style.less`).pipe(less()).pipe(gulp.dest('DIR.build_css'))
 
+});
+
+gulp.task('watch', function(){
+    gulp.watch([`${DIR.less}/**/*.less`], ['less']);
 });
 
 gulp.task('dev', ['less', 'browser-sync']);
