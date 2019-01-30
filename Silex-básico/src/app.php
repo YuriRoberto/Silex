19f8cd64579a7a753dabaed9bd42943aede8f087
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-01-30 10:47:11 -0300

    Debug e VarDumper

--- Silex-básico/src/app.php
+++ Silex-básico/src/app.php
@@ -1,8 +1,12 @@
 <?php
+
 use SON\View\ViewRenderer;
 use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
 $app['debug'] = true;
+
 include __DIR__ . '/services.php';
 include __DIR__ . '/routes.php';
+
 $app->run();
